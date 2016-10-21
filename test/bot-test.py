903cd75dcb767acbc240b3f5af50f459cb911de1
SophieKoonin <sophie.koonin@gmail.com>
2016-10-21 13:39:13 +0100

    test for get_reactions

--- test/bot-test.py
+++ test/bot-test.py
@@ -10,5 +10,13 @@ class TestBot(unittest.TestCase):
         expected_value = ['12345', 'abc123']
         mock_api_call.return_value = {"ts" : "12345", "channel": "abc123"}
 
-        result = bot.post_message("test", "test")
+        result = bot.post_message("test_message", "test_channel")
+        self.assertEqual(result, expected_value)
+
+    @patch("bot.SlackClient.api_call")
+    def test_get_reactions(self, mock_api_call):
+        expected_value=[{"name" : "foo", "users" : ["user1", "user2"]}]
+        mock_api_call.return_value = {"message": {"reactions": expected_value}}
+
+        result = bot.get_reactions("test_timestamp", "test_channel")
         self.assertEqual(result, expected_value)
