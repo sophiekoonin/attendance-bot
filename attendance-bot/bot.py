7ed77d07fc013e5cd224e9c98e9eba73657c3485
SophieKoonin <sophie.koonin@gmail.com>
2016-10-21 12:46:43 +0100

    define a couple of convenience methods for posting and getting reactions

--- attendance-bot/bot.py
+++ attendance-bot/bot.py
@@ -1,10 +1,26 @@
 from slackclient import SlackClient
 import yaml
+import sqlite3
 
 settings = yaml.load(open("settings.yaml"))
 
 token = settings.get("bot-token")
 bot_name = settings.get("bot-name")
-bot_emoji = ":{emoji}:".format(emoji=settings.get("bot-emoji")) #wrap emoji name in colons
+bot_emoji = ":{emoji}:".format(emoji=settings.get("bot-emoji"))  # wrap emoji name in colons
 
 client = SlackClient(token)
+
+# post a message and return the timestamp of the message
+def post_message(message, channel):
+    res = client.api_call(
+        "chat.postMessage", channel=channel, text=message,
+        username=bot_name, icon_emoji=bot_emoji
+    )
+    return [res.get("ts"), res.get("channel")]
+
+def get_reactions(ts, channel):
+    res = client.api_call(
+        "reactions.get", channel=channel, timestamp=ts
+    )
+    return res
+
