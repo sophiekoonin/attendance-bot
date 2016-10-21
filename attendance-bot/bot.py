7d9e9a7bf963a46056c22599ac850485612deaa4
SophieKoonin <sophie.koonin@gmail.com>
2016-10-21 13:31:56 +0100

    move settings file and update bot.py with new location

--- attendance-bot/bot.py
+++ attendance-bot/bot.py
@@ -2,13 +2,14 @@ from slackclient import SlackClient
 import yaml
 import sqlite3
 
-settings = yaml.load(open("settings.yaml"))
+settings = yaml.load(open("../settings.yaml"))
 
 token = settings.get("bot-token")
 bot_name = settings.get("bot-name")
 bot_emoji = ":{emoji}:".format(emoji=settings.get("bot-emoji"))  # wrap emoji name in colons
 
 client = SlackClient(token)
+
 
 # post a message and return the timestamp of the message
 def post_message(message, channel):
@@ -18,9 +19,9 @@ def post_message(message, channel):
     )
     return [res.get("ts"), res.get("channel")]
 
+
 def get_reactions(ts, channel):
     res = client.api_call(
         "reactions.get", channel=channel, timestamp=ts
     )
     return res
-
