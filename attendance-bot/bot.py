c3f03bb540b6f486c418f4c4cd4b961a6656b355
SophieKoonin <sophie.koonin@gmail.com>
2016-10-22 15:08:55 +0100

    need to get channel ID from post to allow adding/getting reactions

--- attendance-bot/bot.py
+++ attendance-bot/bot.py
@@ -3,6 +3,17 @@ import yaml
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
+def schedule(day, hour, mins, func, args):
+    sched = BackgroundScheduler()
+
+    @sched.scheduled_job('cron', day_of_week=day, hour=hour, minute=mins)
+    def scheduled_job():
+        func(*args)
+
+    sched.start()
+    print("Post scheduled for {day} at {hour}:{mins}!".format(day=day, hour=hour, mins=mins))
+
+
 class AttendanceBot(object):
 
     def __init__(self, settings):
@@ -12,15 +23,17 @@ class AttendanceBot(object):
         self.bot_emoji = ":{emoji}:".format(emoji=settings.get("bot-emoji"))  # wrap emoji name in colons
         self.client = SlackClient(token)
         self.channel = settings.get("channel")
-        #schedule the rehearsal message post
-        self.schedule(
-            settings.get("rehearsal-day"),
-            settings.get("post-hour"),
-            settings.get("post-minute"),
-            self.post_message,
-            [settings.get("rehearsal-message"),
-            self.channel]
-        )
+        self.emoji_present = settings.get("emoji-present")
+        self.emoji_absent = settings.get("emoji-absent")
+
+        # # schedule the rehearsal message post
+        # schedule(
+        #     settings.get("rehearsal-day"),
+        #     settings.get("post-hour"),
+        #     settings.get("post-minute"),
+        #     self.post_message_with_reactions,
+        #     [settings.get("rehearsal-message")]
+        # )
 
     # post a message and return the timestamp of the message
     def post_message(self, message):
@@ -28,24 +41,26 @@ class AttendanceBot(object):
             "chat.postMessage", channel=self.channel, text=message,
             username=self.bot_name, icon_emoji=self.bot_emoji
         )
-        return res.get("ts")
+        return [res.get("ts"), res.get("channel")]
 
     # post a message, react to it, and return the timestamp of the message
     def post_message_with_reactions(self, message):
-        ts = self.post_message(message)
+        post_data = self.post_message(message)
+        ts = post_data[0]
+        channel = post_data[1]
+
+        print(self.client.api_call(
+            "reactions.add", channel=channel, timestamp=ts, name=self.emoji_present
+        ))
 
         self.client.api_call(
-            "reactions.add", channel=self.channel, timestamp=ts, name="thumbsup"
-        )
-
-        self.client.api_call(
-            "reactions.add", channel=self.channel, timestamp=ts, name="thumbsdown"
+            "reactions.add", channel=channel, timestamp=ts, name=self.emoji_absent
         )
         return ts
 
-    def get_reactions(self, ts):
+    def get_reactions(self, ts, channel):
         res = self.client.api_call(
-            "reactions.get", channel=self.channel, timestamp=ts
+            "reactions.get", channel=channel, timestamp=ts
         )
         return res.get("message").get("reactions")
 
@@ -54,13 +69,3 @@ class AttendanceBot(object):
             "users.info", user=user_id
         )
         return res.get("user").get("profile").get("real_name")
-
-    def schedule(self, day, hour, mins, func, args):
-        sched = BackgroundScheduler()
-
-        @sched.scheduled_job('cron', day_of_week=day, hour=hour, minute=mins)
-        def scheduled_job():
-            func(*args)
-
-        sched.start()
-        print("Post scheduled for {day} at {hour}:{mins}!".format(day=day, hour=hour, mins=mins))
