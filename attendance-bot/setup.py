01c3015cbf982d0254433cf7860256829b941cc8
SophieKoonin <sophie.koonin@gmail.com>
2016-10-22 15:12:06 +0100

    add setup.py

---
+++ attendance-bot/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='attendance-bot',
+    version='1',
+    packages=['attendance-bot'],
+    package_dir={'': 'attendance-bot'},
+    url='http://www.github.com/SophieKoonin/attendance-bot.git',
+    license='',
+    author='Sophie Koonin',
+    author_email='',
+    description='A Slackbot to record attendance and keep track in a Google spreadsheet'
+)
