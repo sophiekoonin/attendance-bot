54dbd1fe905893941dff0fac4d86b3cccae20bb1
SophieKoonin <sophie.koonin@gmail.com>
2016-10-31 14:19:13 +0000

    app

---
+++ attendance-bot/app.py
@@ -0,0 +1,54 @@
+from flask import Flask
+from flask_slack import Slack
+from bot import AttendanceBot
+import os
+import yaml
+
+app = Flask(__name__)
+slack = Slack(app)
+bot = AttendanceBot(yaml.load(open('../settings.yaml')))
+SLASH_TOKEN = os.environ["SLASH_TOKEN"]
+TEAM_ID = "T2RT9TS8M"
+HELP_TEXT = ("I am the attendance bot! :robot::memo:"
+             "Type /here or /absent followed by full name, date as DD/MM/YY, "
+             "separated by a comma. e.g.:\n"
+             "/here Beyoncé Knowles, 31/10/16\n"
+             "/absent Chaka Khan, 02/01/17")
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+app.add_url_rule('/here', view_func=slack.dispatch)
+app.add_url_rule('/absent', view_func=slack.dispatch)
+
+
+@slack.command('here', token=SLASH_TOKEN,
+               team_id=TEAM_ID, methods=['POST'])
+def member_present(**kwargs):
+    input_text = kwargs.get('text')
+    return process_attendance(input_text, bot.record_presence)
+
+
+@slack.command('absent', token=SLASH_TOKEN,
+               team_id=TEAM_ID, methods=['POST'])
+def member_absent(**kwargs):
+    input_text = kwargs.get('text')
+    return process_attendance(input_text, bot.record_absence)
+
+
+def process_attendance(input_text, attendance_func):
+    if len(input_text) == 0 or 'help' in input_text:
+        return slack.response(HELP_TEXT)
+    input_list = input_text.strip().split(',')
+    name = input_list[0]
+    date = input_list[1]
+    slack_id = bot.get_slack_id(name)
+    if not slack_id:
+        return slack.response("Sorry, I couldn't find anyone with that name. :confused:")
+    attendance_func(slack_id, date)
+    return slack.response(("Thanks! I have updated attendance for {name} on {date}. :thumbsup:").format(name, date))
+
+if __name__ == '__main__':
+    app.run()
