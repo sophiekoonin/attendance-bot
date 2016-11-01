962a646102fa36659acfd11ad9d35c57a32492b4
SophieKoonin <sophie.koonin@gmail.com>
2016-11-01 11:46:10 +0000

    tidy and refactor

--- test/test-bot.py
+++ test/test-bot.py
@@ -14,16 +14,13 @@ class TestBot(unittest.TestCase):
     def setUpClass(cls):
         settings = yaml.load(open("../settings.yaml"))
         cls.bot = AttendanceBot(settings)
-        cur = cls.test_db.cursor()
-        cur.execute("CREATE TABLE if not exists Members(slack_id varchar(255) not null primary key, real_name varchar(255) not null)")
-        cur.execute("CREATE TABLE if not exists Posts(post_timestamp varchar(255) not null primary key, rehearsal_date varchar(255) unique not null, channel_id varchar(255) not null)")
-        cur.execute("CREATE TABLE if not exists Attendance(slack_id varchar(255) references Members(slack_id), post_timestamp varchar(255) references Posts(post_timestamp), Present boolean, primary key (slack_id, rehearsal_date))")
-        dbutils.commit_or_rollback(cls.test_db)
+        cls.bot.create_tables()
 
     def setUp(self):
         self.test_db.cursor().execute("INSERT INTO Members VALUES(%s, %s)", ("12345", "Bobby Tables"))
         self.test_db.cursor().execute("INSERT INTO Posts VALUES(%s, %s, %s)", ("1477908000", "31/10/16", "abc123"))
-        self.test_db.cursor().execute("INSERT INTO Attendance(slack_id, post_timestamp) VALUES(%s, %s)", ("12345", "1477908000"))
+        self.test_db.cursor().execute("INSERT INTO Attendance(slack_id, post_timestamp) VALUES(%s, %s)",
+                                      ("12345", "1477908000"))
         dbutils.commit_or_rollback(self.test_db)
 
     def test_init_func(self):
@@ -57,7 +54,8 @@ class TestBot(unittest.TestCase):
 
     def test_get_latest_post_data(self):
         cur = self.test_db.cursor()
-        cur.execute("insert into posts values('1477908005', '30/10/16', 'abc123'), ('1477908006', '32/10/16', 'abc123'), ('1477908007', '33/10/16', 'abc123')")
+        cur.execute("insert into posts values('1477908005', '30/10/16', 'abc123'), "
+                    "('1477908006', '32/10/16', 'abc123'), ('1477908007', '33/10/16', 'abc123')")
         dbutils.commit_or_rollback(self.test_db)
         expected_value = {"ts": "1477908007", "channel_id": "abc123"}
         result = self.bot.get_latest_post_data()
@@ -76,13 +74,17 @@ class TestBot(unittest.TestCase):
     @patch("bot.SlackClient.api_call")
     def test_get_slack_id_not_present(self, mock_api_call):
         expected_value = "234567"
-        mock_api_call.return_value =   {"members": [{"id": "234567", "real_name": "Bob Loblaw", "deleted": False}, {"id": "345678", "real_name": "Michael Bluth", "deleted": False}, {"id": "101011", "real_name": "GOB Bluth", "deleted": True}]}
+        mock_api_call.return_value = {"members": [{"id": "234567", "real_name": "Bob Loblaw", "deleted": False},
+                                                  {"id": "345678", "real_name": "Michael Bluth", "deleted": False},
+                                                  {"id": "101011", "real_name": "GOB Bluth", "deleted": True}]}
         result = self.bot.get_slack_id("Bob Loblaw")
         self.assertEqual(result, expected_value)
 
     @patch("bot.SlackClient.api_call")
     def test_get_slack_id_non_existent(self, mock_api_call):
-        mock_api_call.return_value =   {"members": [{"id": "234567", "real_name": "Bob Loblaw", "deleted": False}, {"id": "345678", "real_name": "Michael Bluth", "deleted": False}, {"id": "101011", "real_name": "GOB Bluth", "deleted": True}]}
+        mock_api_call.return_value = {"members": [{"id": "234567", "real_name": "Bob Loblaw", "deleted": False},
+                                                  {"id": "345678", "real_name": "Michael Bluth", "deleted": False},
+                                                  {"id": "101011", "real_name": "GOB Bluth", "deleted": True}]}
         result = self.bot.get_slack_id("Buster Bluth")
         self.assertIsNone(result)
 
@@ -96,14 +98,15 @@ class TestBot(unittest.TestCase):
     @patch("bot.SlackClient.api_call")
     def test_update_users(self, mock_api_call):
         expected_value = [("12345",), ("234567",), ("345678",)]
-        mock_api_call.return_value =   {"members": [{"id": "234567", "real_name": "Bob Loblaw", "deleted": False}, {"id": "345678", "real_name": "Michael Bluth", "deleted": False}, {"id": "101011", "real_name": "GOB Bluth", "deleted": True}]}
+        mock_api_call.return_value = {"members": [{"id": "234567", "real_name": "Bob Loblaw", "deleted": False},
+                                                  {"id": "345678", "real_name": "Michael Bluth", "deleted": False},
+                                                  {"id": "101011", "real_name": "GOB Bluth", "deleted": True}]}
         self.bot.update_members()
         result = dbutils.execute_fetchall(self.test_db, "select slack_id from members")
         self.assertEqual(result, expected_value)
 
     def test_record_presence(self):
         expected_value = True
-        cur = self.test_db.cursor()
         self.bot.record_presence("12345", "1477908000")
         query = "select Present from Attendance where slack_id='12345' and post_timestamp = '1477908000'"
         result = dbutils.execute_fetchone(self.test_db, query)[0]
@@ -111,7 +114,6 @@ class TestBot(unittest.TestCase):
 
     def test_record_absence(self):
         expected_value = False
-        cur = self.test_db.cursor()
         self.bot.record_absence("12345", "1477908000")
         query = "select Present from Attendance where slack_id='12345' and post_timestamp = '1477908000'"
         result = dbutils.execute_fetchone(self.test_db, query)[0]
@@ -145,12 +147,16 @@ class TestBot(unittest.TestCase):
 
     @patch("bot.SlackClient.api_call")
     @patch("bot.AttendanceBot.get_reactions")
-    def test_process_attendance(self, mock_get_reactions, mock_api_call,):
+    def test_process_attendance(self, mock_get_reactions, mock_api_call, ):
         expected_value = [(None,), (True,), (True,), (True,), (False,)]
-        mock_api_call.return_value = {"members": [{"id": "23456", "real_name": "Bob Loblaw", "deleted": False}, {"id": "34567", "real_name": "Michael Bluth", "deleted": False}, {"id": "101011", "real_name": "GOB Bluth", "deleted": True}]}
-        mock_get_reactions.return_value = [{"name":"thumbsup", "users":["12345", "23456", "45678"]},{"name":"thumbsdown", "users":["34567"]}]
+        mock_api_call.return_value = {"members": [{"id": "23456", "real_name": "Bob Loblaw", "deleted": False},
+                                                  {"id": "34567", "real_name": "Michael Bluth", "deleted": False},
+                                                  {"id": "101011", "real_name": "GOB Bluth", "deleted": True}]}
+        mock_get_reactions.return_value = [{"name": "thumbsup", "users": ["12345", "23456", "45678"]},
+                                           {"name": "thumbsdown", "users": ["34567"]}]
         cur = self.test_db.cursor()
-        cur.execute("insert into members values ('23456', 'Tobias Funke'),('34567', 'GOB Bluth'),('45678', 'Buster Bluth'), ('56789', 'George Michael Bluth')")
+        cur.execute("insert into members values ('23456', 'Tobias Funke'),('34567', 'GOB Bluth'),"
+                    "('45678', 'Buster Bluth'), ('56789', 'George Michael Bluth')")
         dbutils.commit_or_rollback(self.test_db)
         self.bot.process_attendance()
         cur.execute("select present from attendance where post_timestamp = '1477908000'")
@@ -165,8 +171,9 @@ class TestBot(unittest.TestCase):
                                                                  ("1479908000", "10/11/16", "abc123"),
                                                                  ("1487908000", "15/11/16", "abc123"),
                                                                  ("1497908000", "25/11/16", "abc123")))
-        cur.execute(
-            "insert into attendance(slack_id, post_timestamp) select m.slack_id, p.post_timestamp from members as m, posts as p on conflict do nothing")
+        query = ("insert into attendance(slack_id, post_timestamp) select m.slack_id, "
+                 "p.post_timestamp from members as m, posts as p on conflict do nothing")
+        cur.execute(query)
         cur.executemany("UPDATE attendance SET present=(%s) WHERE slack_id=(%s) AND post_timestamp=(%s)",
                         ((True, '12345', '1477908000'), (True, '34567', '1477908000'), (True, '45678', '1477908000'),
                          (False, '56789', '1477908000'),
