006239e668c0d867bd3a5ce583ee7b9e8d01d363
SophieKoonin <sophie.koonin@gmail.com>
2016-11-01 14:44:04 +0000

    missing password param

--- attendance-bot/dbutils.py
+++ attendance-bot/dbutils.py
@@ -8,6 +8,7 @@ def connect_to_db():
     return psycopg2.connect(
         database=url.path[1:],
         user=url.username,
+        password=url.password,
         host=url.hostname,
         port=url.port
     )
