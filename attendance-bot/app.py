e4d7e755e6c10cf29150c60ca02b4f9df1b1cc2b
SophieKoonin <sophie.koonin@gmail.com>
2016-11-01 14:48:04 +0000

    fix app.run call with port

--- attendance-bot/app.py
+++ attendance-bot/app.py
@@ -61,4 +61,5 @@ def process_attendance(input_text, attendance_func):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
